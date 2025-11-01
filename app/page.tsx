6e7b208582dcb6ee7a03c6a902cb7572ddd7d15b
Ibrahim Mohamed <ibrahim11903145@miuegypt.edu.eg>
2025-11-01 16:48:56 +0200

    HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import EventCard from '@/components/EventCard';
+import Explorebtn from '@/components/Explorebtn'
+
+
+
+const events= [
+  {image: '/images/event1.png',
+     title: 'Event 1',
+     slug: 'event-1',
+     location:'location 1',
+     date:'Date-1',
+     time:'Time-1'
+    },
+  {image: '/images/event2.png', title: 'Event 2'},
+  
+];
 
 const page = () => {
   return (
-    <h1>Welcome To Next.js 16</h1>
+    <section>
+    <h1 className="text-center ">The Hub For Every Dev <br/> Event You Can't Miss</h1>
+     <p className='text-center mt-5'>Hackthons, Meetups, and Conferences, All in One Place</p>
+     <Explorebtn/>
+        <div className="mt-20 space-y-7">
+         <h3>Featured Events</h3>
+
+          <ul className='events'>
+           {events. map((event) => (
+              <li key={event.title}>
+              <EventCard {...event}/>
+              </li>
+           ))}
+          </ul>
+         
+        </div>
+     
+     
+     
+     </section>
+
   )
 }
 
